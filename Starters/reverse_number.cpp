285441951ad229706e6b5f3b814601bce880fb20
km12 <125912478+km-devlab@users.noreply.github.com>
2026-02-13 17:19:53 +0530

    Implement reverse number logic in C++

--- Starters/reverse_number.cpp
+++ Starters/reverse_number.cpp
@@ -29,3 +29,24 @@ O(1)
 Starters 🥗 → Number Basics
 */
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int reverse = 0;
+
+    while (n > 0)
+    {
+        int digit = n % 10;
+        reverse = reverse * 10 + digit;
+        n /= 10;
+    }
+
+    cout << reverse;
+
+    return 0;
+}
