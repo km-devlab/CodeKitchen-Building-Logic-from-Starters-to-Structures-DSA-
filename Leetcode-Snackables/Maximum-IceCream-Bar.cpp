0beb1c90d8d425fcb54d6fee1e15b39120a41642
km12 <125912478+km-devlab@users.noreply.github.com>
2026-06-21 23:59:16 +0530

    Update Maximum-IceCream-Bar.cpp

--- Leetcode-Snackables/Maximum-IceCream-Bar.cpp
+++ Leetcode-Snackables/Maximum-IceCream-Bar.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        int sum=0;
+        int count=0;
+        sort(costs.begin(),costs.end());
+        for(int i=0;i<costs.size();i++)
+        {
+            if(costs[0]>coins)
+            {
+                return 0;
+            }
+            sum+=costs[i];
+            count++;
+            if(sum>coins)
+            {
+             return count-1;
+            }
+        }
+        return count;
+    }
+};
