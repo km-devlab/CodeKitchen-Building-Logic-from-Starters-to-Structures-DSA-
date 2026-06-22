ed0be7bab4f04bb6d3580758523f859ef3e25ff6
km12 <125912478+km-devlab@users.noreply.github.com>
2026-06-22 23:31:20 +0530

    Update Maximum-Number-of-Ballons.cpp

--- Leetcode-Snackables/Maximum-Number-of-Ballons.cpp
+++ Leetcode-Snackables/Maximum-Number-of-Ballons.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+       unordered_map<char,int> frequency;
+       for(char ch:text)
+       {
+        frequency[ch]++;
+       }
+       int b=frequency['b'];
+       int a=frequency['a'];
+       int l=frequency['l']/2;
+       int o=frequency['o']/2;
+       int n=frequency['n'];
 
+       return min({b,a,l,n,o});
+    }
+};
