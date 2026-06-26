85367aa829f80169bc0a4b25f62df0899fcc1cc4
km12 <125912478+km-devlab@users.noreply.github.com>
2026-06-26 22:55:10 +0530

    Update Maximum-Product-of-3-Numbers.cpp

--- Leetcode-Snackables/Maximum-Product-of-3-Numbers.cpp
+++ Leetcode-Snackables/Maximum-Product-of-3-Numbers.cpp
@@ -1,1 +1,10 @@
-
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int productoflastthree=nums[n-1]*nums[n-2]*nums[n-3];
+        int productoffirsttwo=nums[0]*nums[1]*nums[n-1];
+        return max(productoflastthree,productoffirsttwo);
+    }
+};
