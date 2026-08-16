dd4e9f49e845610e887578f85e7564643150dd83
km12 <125912478+km-devlab@users.noreply.github.com>
2026-08-16 22:28:10 +0530

    Update prime_no_between_range.cpp

--- Starters/prime_no_between_range.cpp
+++ Starters/prime_no_between_range.cpp
@@ -1,1 +1,26 @@
 
+#include<iostream>
+using namespace std;
+int main()
+{
+int start,end;
+cin>>start>>end;
+
+for(int n=start;n<=end;n++)
+{
+bool prime=true;
+  if(n<=1)
+    prime=false;
+for(int i=2;i*i<=n;i++){
+if(n%i==0)
+{
+prime=false;
+break;
+}
+}
+if(prime)
+cout<<n<<" ";
+}
+
+return 0;
+}
