f3fa992d81ad10034ba24fc5d814485dda467a45
km12 <125912478+km-devlab@users.noreply.github.com>
2026-08-26 00:16:55 +0530

    Update Smallest_Missing_Multiple_Of_k.cpp

--- Leetcode-Snackables/Smallest_Missing_Multiple_Of_k.cpp
+++ Leetcode-Snackables/Smallest_Missing_Multiple_Of_k.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        if(find(nums.begin(),nums.end(),k)==nums.end()) return k;
 
+        auto max_value=max_element(nums.begin(),nums.end());
+        int prod=1;
+        for(int i=1;i<=*max_value;i++)
+        {
+            prod=k*i;
+            if(find(nums.begin(),nums.end(),prod)==nums.end())
+            {return prod;}
+            else{
+                continue;
+            }
+        }
+        return *max_value+k;
+}
+};
