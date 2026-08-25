116f56c8724c4c5c307d1a60a388c1bc17fb7897
km12 <125912478+km-devlab@users.noreply.github.com>
2026-08-26 00:28:14 +0530

    Update Divide_Array_Into_Equal_Pairs.cpp

--- Leetcode-Snackables/Divide_Array_Into_Equal_Pairs.cpp
+++ Leetcode-Snackables/Divide_Array_Into_Equal_Pairs.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int,int> pairs;
+        for(int x:nums)
+        {
+            pairs[x]++;
+        }
+        for(auto x:pairs)
+        {
+            if(x.second%2!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
